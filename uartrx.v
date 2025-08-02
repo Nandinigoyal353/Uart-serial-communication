186e90e065f431280b9158654e338df65d2b3ec8
Nandinigoyal353 <nandinigoyalgghs33@gmail.com>
2025-08-02 09:00:56 +0530

    Update uartrx.v

--- uartrx.v
+++ uartrx.v
@@ -1,60 +1,46 @@
-module uartrx #(
-    parameter clk_freq = 1000000, // Clock Frequency = 1MHz
-    parameter baud_rate = 9600, // Baud Rate = 9600
-    parameter IDLE = 1'b0,
-    parameter START = 1'b1
-    ) (
-    input clk, rst,
+module uart_rx (
+    input clk,
+    input rst,
     input rx,
-    output reg done,
-    output reg [7:0] rxdata
-    );
-    localparam clk_count = (clk_freq/baud_rate); // Ratio between clock frequency and baud rate
-    
-    integer count = 0; // Used for UART Clock Generation
+    input tick,
+    output reg [7:0] rx_data,
+    output reg rx_done
+);
 
-    reg uclk = 0; // UART Clock
-    reg state;
-    reg [2:0] counter;
+    reg [3:0] bit_index;
+    reg [7:0] data_reg;
+    reg [1:0] state;
 
-    // UART Clock Generation
-    always @(posedge clk) begin
-        if(count < clk_count/2)
-            count <= count + 1;
-        else begin
-            count <= 0;
-            uclk <= ~uclk;
-        end
-    end
-
-    always @(posedge uclk) begin
+    always @(posedge clk or posedge rst) begin
         if (rst) begin
-            done <= 0;
-            rxdata <= 0;
-            counter <= 0;
-            state <= IDLE;
-        end
-        else begin
-            case (state)
-                IDLE: begin
-                    rxdata <= 0;
-                    counter <= 0;
-                    done <= 0;
-                    if(rx == 1'b0)
-                        state <= START;
-                    else
-                        state <= IDLE;
+            bit_index <= 0;
+            data_reg <= 0;
+            rx_data <= 0;
+            rx_done <= 0;
+            state <= 0;
+        end else begin
+            rx_done <= 0;
+            case(state)
+                0: begin // Wait for start bit
+                    if (~rx) state <= 1;
                 end
-                START: begin
-                    counter <= counter + 1;
-                    rxdata <= {rx, rxdata[7:1]};
-                    if(counter == 3'h7) begin
-                        counter <= 0;
-                        done <= 1'b1;
-                        state <= IDLE;
+                1: begin // Read 8 bits
+                    if (tick) begin
+                        data_reg[bit_index] <= rx;
+                        bit_index <= bit_index + 1;
+                        if (bit_index == 7) begin
+                            state <= 2;
+                            bit_index <= 0;
+                        end
                     end
                 end
-                default: state <= IDLE;
+                2: begin // Stop bit
+                    if (tick) begin
+                        rx_data <= data_reg;
+                        rx_done <= 1;
+                        state <= 0;
+                    end
+                end
             endcase
         end
     end
