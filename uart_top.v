a99cce86cbbb3fd246c645347bf404e8913b0a0b
Nandinigoyal353 <nandinigoyalgghs33@gmail.com>
2025-08-02 09:01:39 +0530

    Update uart_top.v

--- uart_top.v
+++ uart_top.v
@@ -1,14 +1,37 @@
 module uart_top (
-    input clk, rst,
-    input rx,
-    input [7:0] dintx,
-    input newd,
+    input clk,
+    input rst,
+    input tx_start,
+    input [7:0] tx_data,
     output tx,
-    output [7:0] doutrx,
-    output donetx, donerx
+    input rx,
+    output [7:0] rx_data,
+    output rx_done
+);
+
+    wire tick;
+
+    baud_gen baud_inst (
+        .clk(clk),
+        .rst(rst),
+        .tick(tick)
     );
 
-    uarttx UTX(clk, rst, newd, dintx, tx, donetx);
+    uart_tx tx_inst (
+        .clk(clk),
+        .rst(rst),
+        .tx_start(tx_start),
+        .tx_data(tx_data),
+        .tick(tick),
+        .tx(tx)
+    );
 
-    uartrx URX(clk, rst, rx, donerx, doutrx);
+    uart_rx rx_inst (
+        .clk(clk),
+        .rst(rst),
+        .rx(rx),
+        .tick(tick),
+        .rx_data(rx_data),
+        .rx_done(rx_done)
+    );
 endmodule
